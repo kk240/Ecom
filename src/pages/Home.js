153cee7cc05b1877a2d9694659d5463437c68d47
Kundan <kundanss22@gmail.com>
2024-08-05 09:52:27 +0530

    project setup with react and  react router

---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Navbar from '../features/navbar/Navbar'
+import ProductList from '../features/product-list/ProductList'
+
+const Home = () => {
+    return (
+        <Navbar children={<ProductList />} />
+    )
+}
+
+export default Home
